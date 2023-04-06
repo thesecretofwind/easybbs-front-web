a366fc737e6e2fd1f0b584de94bafba074ff1421
thesecretofwind <2586404691@qq.com>
2023-04-06 22:24:03 +0800

    fix(service):新增http状态码为901时，更新状态管理库的userId

--- src/app/interceptor/htt.interceptor.ts
+++ src/app/interceptor/htt.interceptor.ts
@@ -4,9 +4,15 @@ import {
   HttpHandler,
   HttpEvent,
   HttpInterceptor,
-  HttpHeaders
+  HttpHeaders,
+  HttpEventType,
+  HttpResponse
 } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Observable, of } from 'rxjs';
+import { map, mergeMap } from 'rxjs/operators'
+import { AppState } from '../store';
+import { Store } from '@ngrx/store';
+import { userInfoAction } from '../store/actions/userInfo.action';
 
 const httpOptions = {
   headers: new HttpHeaders({
@@ -18,9 +24,20 @@ const httpOptions = {
 @Injectable()
 export class HttInterceptor implements HttpInterceptor {
 
-  constructor() {}
+  constructor(private store: Store<AppState>) {}
 
   intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
-    return next.handle(request);
+    return next.handle(request).pipe(
+      mergeMap( (res: HttpEvent<any>) => {
+        if (res instanceof HttpResponse) {
+          const data = res.body.data;
+          const code = data.code;
+          if (code === 901) {
+            this.store.dispatch(userInfoAction({userId: ''}));
+          }
+        }
+        return of(res);
+      })
+    );
   }
 }
